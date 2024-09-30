e70fba12d3fa04013422384468addd0a266859a1
Silvia Ferrari <silvia.ferrari369@gmail.com>
2024-09-30 12:44:29 +0200

    i don't know

--- exercises/src/main/java/com/SilviaFerrari/exercices/arrays/Splitter.java
+++ exercises/src/main/java/com/SilviaFerrari/exercices/arrays/Splitter.java
@@ -1,9 +1,15 @@
 package com.SilviaFerrari.exercices.arrays;
 
 public class Splitter {
-    /*public static int[] splitter(int input){
+    public static int[] splitter(int input){
+        String str = Integer.toString(input);
+        int[] digit = new int[str.length()];
 
-    }*/
+        for(int i = 0; i < str.length(); i++){
+            digit[i] = Character.getNumericValue(str.charAt(i));
+        }
+        return digit;
+    }
     public static void main(String[] args) {
 
     }
