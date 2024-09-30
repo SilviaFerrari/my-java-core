4e6d98bb463bc698da96ebb8ed17046268329a22
Silvia Ferrari <40138805+SilviaFerrari@users.noreply.github.com>
2024-09-30 12:03:30 +0200

    Create Splitter.java

---
+++ exercises/src/main/java/com/SilviaFerrari/exercices/arrays/Splitter.java
@@ -0,0 +1,10 @@
+package com.SilviaFerrari.exercices.arrays;
+
+public class Splitter {
+    /*public static int[] splitter(int input){
+
+    }*/
+    public static void main(String[] args) {
+
+    }
+}
