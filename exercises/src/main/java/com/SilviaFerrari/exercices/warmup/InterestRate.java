88d8b1cd2d381a3b2bfd91dfe3ad8ac79d44ff13
Silvia Ferrari <40138805+SilviaFerrari@users.noreply.github.com>
2024-09-30 11:58:31 +0200

    Create InterestRate.java

---
+++ exercises/src/main/java/com/SilviaFerrari/exercices/warmup/InterestRate.java
@@ -0,0 +1,11 @@
+package com.SilviaFerrari.exercices.warmup;
+
+public class InterestRate {
+    public static void main(String[] args) {
+        int b = 1000;
+        for(int i = 1; i < 4; i++){
+            b += (b * 5) / 100;
+            System.out.println("The balance after " + i + " year is " + b);
+        }
+    }
+}
